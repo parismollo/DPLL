8dc277ae8ce6d8f07d68ac806f151208c5c049d5
paris <parismollochristondis@gmail.com>
2022-10-26 21:08:30 +0200

    answer question 5

--- mp1/dpll.ml
+++ mp1/dpll.ml
@@ -31,8 +31,7 @@ let exemple_7_4 = [[1;2;3];[-1;2;3];[3];[1;-2;-3];[-1;-2;-3];[-3]]
 let exemple_7_8 = [[1;-2;3];[1;-3];[2;3];[1;-2]]
 let systeme = [[-1;2];[1;-2];[1;-3];[1;2;3];[-1;-2]]
 let coloriage = [[1;2;3];[4;5;6];[7;8;9];[10;11;12];[13;14;15];[16;17;18];[19;20;21];[-1;-2];[-1;-3];[-2;-3];[-4;-5];[-4;-6];[-5;-6];[-7;-8];[-7;-9];[-8;-9];[-10;-11];[-10;-12];[-11;-12];[-13;-14];[-13;-15];[-14;-15];[-16;-17];[-16;-18];[-17;-18];[-19;-20];[-19;-21];[-20;-21];[-1;-4];[-2;-5];[-3;-6];[-1;-7];[-2;-8];[-3;-9];[-4;-7];[-5;-8];[-6;-9];[-4;-10];[-5;-11];[-6;-12];[-7;-10];[-8;-11];[-9;-12];[-7;-13];[-8;-14];[-9;-15];[-7;-16];[-8;-17];[-9;-18];[-10;-13];[-11;-14];[-12;-15];[-13;-16];[-14;-17];[-15;-18]]
-
-
+(* let example_new = [[2; 3]; [2; 3]; [-1; 3; 2]; [-3; -1]; [1; -3; -1]; [1]];; *)
 (* simplifie : int -> int list list -> int list list 
    Applique la simplification de l'ensemble des clauses en mettant
    le littéral l à vrai
@@ -72,8 +71,10 @@ let rec solveur_split clauses interpretation =
   | _    -> branche
 
 (* Tests solveur_split *)
-(* let () = print_modele (solveur_split systeme []) *)
-(* let () = print_modele (solveur_split coloriage []) *)
+(* let () = print_modele (solveur_split systeme []) 
+let () = print_modele (solveur_split coloriage []) *)
+(* let () = print_modele (solveur_split example_new []) *)
+
     
 (* unitaire : int list list -> int
     - si `clauses' contient au moins une clause unitaire, retourne
@@ -171,8 +172,9 @@ let rec solveur_dpll_rec clauses interp =
   ;;
 
 (* tests *)
-(* let () = print_modele (solveur_dpll_rec systeme [])  *)
-(* let () = print_modele (solveur_dpll_rec coloriage []) *)
+(* let () = print_modele (solveur_dpll_rec systeme [])
+let () = print_modele (solveur_dpll_rec coloriage []) *)
+(* let () = print_modele (solveur_dpll_rec example_new []) *)
 
 let () =
   let clauses = Dimacs.parse Sys.argv.(1) in
